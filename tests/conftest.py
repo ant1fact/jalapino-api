d3d6ff63d1fe9f32e0eba2ed0cfcd44013256d5e
ant1fact <pacsuta.david@gmail.com>
2022-02-15 23:08:24 +0100

    Add more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,15 @@ import pytest
 from api import create_app
 from api.config import TestConfig
 
+def make_auth_header(role: str) -> str:
+    assert role in {'customer', 'restaurant'}
+    tokens = {
+        'customer': TestConfig.CUSTOMER_TOKEN,
+        'restaurant': TestConfig.RESTAURANT_TOKEN
+    }
+    return {'Authorization': f'Bearer {tokens[role]}'}
+
+
 @pytest.fixture()
 def app():  # sourcery skip: inline-immediately-yielded-variable
     app = create_app(config=TestConfig)
--- tests/test_api_rbac.py
+++ tests/test_api_rbac.py
@@ -1,11 +1,12 @@
 from api.models import Restaurant
-from conftest import create_app
+from conftest import create_app, make_auth_header
 
 app = create_app()
 
+
 def test_pass_get_restaurants(client):
     response = client.get('/restaurants')
-    assert response.status_code == 200    
+    assert response.status_code == 200
     restaurants = response.get_json()
 
     assert len(restaurants) == 3
@@ -18,11 +19,13 @@ def test_pass_get_restaurants(client):
             assert 'auth0_id' not in restaurant
             assert Restaurant.query.get(restaurant['id']).auth0_id
 
+
 def test_fail_get_restaurants(client):
     # GET /restaurant (singular instead of plural)
     response = client.get('/restaurant')
     assert response.status_code == 404
     assert response.get_json() is None
+
 
 def test_pass_get_restaurant(client):
     response = client.get('/restaurants/1')
@@ -35,29 +38,35 @@ def test_pass_get_restaurant(client):
     with app.app_context():
         assert Restaurant.query.get(1).categories[0].items[0].ingredients
 
+
 def test_fail_get_restaurant(client):
     response = client.get('/restaurants/somestring')
     assert response.status_code == 404
+
 
 def test_pass_create_customer(client):
     new_customer_data = {
         "name": "TEST",
         "email": "test@test.com",
         "phone": "1-234-567890",
-        "address": "999 Test St, Test City, TE"
+        "address": "999 Test St, Test City, TE",
     }
-    response = client.post('/customers', json={}})
-    assert response.status_code == 401
+    response = client.post(
+        '/customers', json=new_customer_data, headers=make_auth_header('customer')
+    )
+    assert response.status_code == 201
+
 
 def test_fail_create_customer(client):
     new_customer_data = {
         "name": "TEST",
         "email": "test@test.com",
         "phone": "1-234-567890",
-        "address": "999 Test St, Test City, TE"
+        "address": "999 Test St, Test City, TE",
     }
-    response = client.post('/customers', json={}})
+    response = client.post('/customers', json={})
     assert response.status_code == 401
+
 
 def test_fail_get_customers(client):
     response = client.get('/customers')
